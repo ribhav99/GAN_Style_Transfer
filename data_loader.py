6557db835609b34ee7a3e46a84f4adef8da2f915
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-06-18 16:08:05 +0530

    data loaders works for cartoons. need to discuss about humans

--- data_loader.py
+++ data_loader.py
@@ -3,35 +3,43 @@ import PIL.Image
 from PIL.ImageOps import grayscale
 from skimage.transform import resize
 import os
+from tqdm import tqdm
 
 human_faces_data = []
 human_faces_target = []
 cartoon_data = []
 
 human_faces_data_directory = ""
-cartoon_data_directory = ""
+cartoon_data_directory = "/Users/RibhavKapur/Downloads/cartoonset10k"
 
 image_dimensions = (256, 256)
-cartoon_dimensions = (32, 32)
+cartoon_dimensions = (128, 128)
 
 def load_human_faces_data(directory):
-    for file in os.scandir(directory):
+    for file in tqdm(os.listdir(directory)):
         try:
-            image = PIL.Image.open(file) # or data_directory + "/" + file.  ALSO, convert to numpy array or not?
-            image = resize(image, ouputsize=image_dimensions)
-            human_faces_data.append(image)
-            human_faces_target.append(1)
+            if file.endswith(".png") or file.endswith(".jpg"):
+                image = np.asarray(PIL.Image.open(human_faces_data_directory + "/" + file))
+                image = resize(image, output_shape=image_dimensions)
+                human_faces_data.append(image)
+                human_faces_target.append(1)
         except:
             pass
 
 def load_cartoon_data(directory):
-    for file in os.scandir(directory):
-        try:
-            image = PIL.Image.open(file) # or data_directory + "/" + file.  ALSO, convert to numpy array or not?
-            image = resize(image, ouputsize=cartoon_dimensions)
+    """
+    This works for data downloaded from: https://google.github.io/cartoonset/
+    Adds all the images, and resizes them to desired size, to an array
+    """
+    for file in tqdm(os.listdir(directory)):
+        if file.endswith(".png") or file.endswith(".jpg"):
+            image = np.asarray(PIL.Image.open(cartoon_data_directory + "/" + file))
+            image = resize(image, output_shape=cartoon_dimensions)
             cartoon_data.append(image)
-        except:
-            pass
 
-def gather_cartoon_data(directory):
-    pass
+
+
+#TESTING DELETE LATER XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+load_cartoon_data(cartoon_data_directory)
+print(len(cartoon_data))
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
