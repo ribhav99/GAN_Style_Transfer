b0197faddb21d8c8839019967420d583345879ec
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-06-18 12:01:54 +0530

    first draft of generator

--- GeneratorAndDiscriminator.py
+++ GeneratorAndDiscriminator.py
@@ -26,8 +26,8 @@ class Discriminator(nn.Module):
         self.conv4 = nn.Conv2d(features_d * 4, features_d * 8, kernel_size, stride=2, padding=1)
         self.conv5 = nn.Conv2d(features_d * 8, 1, kernel_size, stride=2, padding=1)
 
-        x = torch.rand(image_dimensions).view(-1, 1, image_dimensions[0], image_dimensions[1])
-        self.conv(x)
+        fake_data = torch.rand(image_dimensions).view(-1, 1, image_dimensions[0], image_dimensions[1])
+        self.conv(fake_data)
         
         self.dense1 = nn.Linear(self._linear_dim, 512)
         self.dense2 = nn.Linear(512, 1)
@@ -50,8 +50,53 @@ class Discriminator(nn.Module):
         x = self.conv(x)
         x = x.view(-1, self._linear_dim)
         x = F.relu(self.dense1(x))
-        x = F.relu(self.dense2(x))
+        x = self.dense2(x)
         x = torch.sigmoid(x)
+        return x
+
+
+
+class Generator(nn.Module):
+
+    def __init__(self, features_g, kernel_size):
+        super().__init__()
+        self._linear_dim = None
+
+        #takes in grayscale images
+        self.conv1 = nn.ConvTranspose2d(1, features_g, kernel_size, stride=2, padding=1)
+        self.conv2 = nn.ConvTranspose2d(features_g, features_g * 16, kernel_size, stride=2, padding=1)
+        self.conv3 = nn.ConvTranspose2d(features_g * 16, features_g * 8, kernel_size, stride=2, padding=1)
+        self.conv4 = nn.ConvTranspose2d(features_g * 8, features_g * 4, kernel_size, stride=2, padding=1)
+        self.conv5 = nn.ConvTranspose2d(features_g * 4, features_g * 2, kernel_size, stride=2, padding=1)
+        self.conv6 = nn.ConvTranspose2d(features_g * 2, features_g, kernel_size, stride=2, padding=1)
+
+
+        fake_data = torch.rand(image_dimensions).view(-1, 1, cartoon_dimensions[0], cartoon_dimensions[1])
+        self.conv(fake_data)
+
+        self.dense1 = nn.Linear(self._linear_dim, (image_dimensions[0] * image_dimensions[1])//2)
+        self.dense2 = nn.Linear((image_dimensions[0] * image_dimensions[1])//2, image_dimensions[0] * image_dimensions[1])
+    
+    def conv(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), max_pool)
+        x = F.max_pool2d(F.relu(self.conv2(x)), max_pool)
+        x = F.max_pool2d(F.relu(self.conv3(x)), max_pool)
+        x = F.max_pool2d(F.relu(self.conv4(x)), max_pool)
+        x = F.max_pool2d(F.relu(self.conv5(x)), max_pool)
+        x = F.max_pool2d(F.relu(self.conv6(x)), max_pool)
+
+        if self._linear_dim is None:
+            self._linear_dim = np.prod(x[0].shape)
+        
+        return x
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(-1, self._linear_dim)
+        print(x.shape)
+        print(self._linear_dim)
+        x = F.relu(self.dense1(x))
+        x = F.relu(self.dense2(x))
         return x
 
 #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -64,4 +109,11 @@ x = torch.rand(image_dimensions).view(-1, 1, image_dimensions[0], image_dimensio
 
 disciminator = Discriminator(features_d, kernel_size)
 print(disciminator(x))
+
+
+features_g = 2
+cartoon_dimensions = (32, 32)
+y = torch.rand(image_dimensions).view(-1, 1, cartoon_dimensions[0], cartoon_dimensions[1])
+generator = Generator(features_g, kernel_size)
+y = generator(y).view(-1, image_dimensions[0], image_dimensions[1])
 #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
