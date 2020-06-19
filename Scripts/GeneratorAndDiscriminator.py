b9a89df0f2c0e9ca32421c064077b2a0d1d00861
Gerald.Shen <gerald.shen@mail.utoronto.ca>
2020-06-19 11:56:00 -0400

    modifed generator
    commented out some stuff here

--- Scripts/GeneratorAndDiscriminator.py
+++ Scripts/GeneratorAndDiscriminator.py
@@ -26,8 +26,8 @@ class Discriminator(nn.Module):
         self.conv4 = nn.Conv2d(features_d * 4, features_d * 8, kernel_size, stride=2, padding=1)
         self.conv5 = nn.Conv2d(features_d * 8, 1, kernel_size, stride=2, padding=1)
 
-        fake_data = torch.rand(image_dimensions).view(-1, image_dimensions[2], image_dimensions[0], image_dimensions[1])
-        self.conv(fake_data)
+# fake_data = torch.rand(image_dimensions).view(-1, image_dimensions[2], image_dimensions[0], image_dimensions[1])
+# self.conv(fake_data)
         
         self.dense1 = nn.Linear(self._linear_dim, 512)
         self.dense2 = nn.Linear(512, 1)
@@ -72,8 +72,8 @@ class Generator(nn.Module):
         self.trans_conv5 = nn.ConvTranspose2d(features_g * 2, features_g, kernel_size, stride=2, padding=1)
 
 
-        fake_data = torch.rand(cartoon_dimensions).view(-1, cartoon_dimensions[2], cartoon_dimensions[0], cartoon_dimensions[1])
-        self.conv(fake_data)
+# fake_data = torch.rand(cartoon_dimensions).view(-1, cartoon_dimensions[2], cartoon_dimensions[0], cartoon_dimensions[1])
+# self.conv(fake_data)
 
         self.dense1 = nn.Linear(self._linear_dim, (image_dimensions[0] * image_dimensions[1] * image_dimensions[2])//2)
         self.dense2 = nn.Linear((image_dimensions[0] * image_dimensions[1] * image_dimensions[2])//2, image_dimensions[0] * image_dimensions[1] * image_dimensions[2])
@@ -102,21 +102,21 @@ class Generator(nn.Module):
 
 
 #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#THESE VALUES FOR TESTING. REMOVE THEM
-features_d = 64
-kernel_size = 3
+# THESE VALUES FOR TESTING. REMOVE THEM
+# features_d = 64
+# kernel_size = 3
 image_dimensions = (218, 178, 3)
-max_pool = (2,2)
-x = torch.rand(image_dimensions).view(-1, 3, image_dimensions[0], image_dimensions[1])
+# max_pool = (2,2)
+# x = torch.rand(image_dimensions).view(-1, 3, image_dimensions[0], image_dimensions[1])
 
-disciminator = Discriminator(features_d, kernel_size)
-print(disciminator(x))
+# disciminator = Discriminator(features_d, kernel_size)
+# print(disciminator(x))
 
 
-features_g = 2
+# features_g = 2
 cartoon_dimensions = (128, 128, 3)
-y = torch.rand(cartoon_dimensions).view(-1, cartoon_dimensions[2], cartoon_dimensions[0], cartoon_dimensions[1])
-generator = Generator(features_g, kernel_size)
-y = generator(y).view(-1, image_dimensions[0], image_dimensions[1], image_dimensions[2])
-print(y.shape)
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# y = torch.rand(cartoon_dimensions).view(-1, cartoon_dimensions[2], cartoon_dimensions[0], cartoon_dimensions[1])
+# generator = Generator(features_g, kernel_size)
+# y = generator(y).view(-1, image_dimensions[0], image_dimensions[1], image_dimensions[2])
+# print(y.shape)
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
