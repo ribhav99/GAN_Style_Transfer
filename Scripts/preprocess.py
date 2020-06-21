297e35374899a1431156f58283adc20d2fcf3703
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-06-21 21:34:07 +0530

    moved files

--- Scripts/preprocess.py
+++ Scripts/preprocess.py
@@ -2,37 +2,38 @@ import numpy as np
 from skimage.transform import resize
 import os
 from skimage import io
-from train import args
 
 
-### Making text files for all image names for cartoon faces
-root_dir = args.cartoon_data_root_path
-name_list = []
-for root, dirs, files in os.walk(root_dir):
-    for file in files:
-        file_dir = os.path.join(root, file)
-        if file_dir.endswith(".png"):
-            # image = io.imread(file_dir)
-            # image = resize(image, (128,128))
-            # io.imsave("/Users/gerald/Desktop/GAN datasets/cartoonfaces/" + file ,image)
-            name_list.append(file)
-print(len(name_list))
-with open("/content/drive/My Drive/CSC420Project/cartoon.txt", 'w') as f:
-    for item in name_list:
-        f.write(item + "\n")
+def make_human_text_file(args):
+    ### Making text files for all image names for cartoon faces
+    root_dir = args.cartoon_data_root_path
+    name_list = []
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            file_dir = os.path.join(root, file)
+            if file_dir.endswith(".png"):
+                # image = io.imread(file_dir)
+                # image = resize(image, (128,128))
+                # io.imsave("/Users/gerald/Desktop/GAN datasets/cartoonfaces/" + file ,image)
+                name_list.append(file)
 
-### Making text files for all image names for human faces
-root_dir = args.human_data_root_path
-name_list = []
-for root, dirs, files in os.walk(root_dir):
-    for file in files:
-        file_dir = os.path.join(root, file)
-        if file_dir.endswith(".jpg"):
-            # image = io.imread(file_dir)
-            # image = resize(image, (128,128))
-            # io.imsave("/Users/gerald/Desktop/GAN datasets/cartoonfaces/" + file ,image)
-            name_list.append(file)
+    with open("/content/drive/My Drive/CSC420Project/cartoon.txt", 'w') as f:
+        for item in name_list:
+            f.write(item + "\n")
 
-with open("/content/drive/My Drive/CSC420Project/human.txt", 'w') as f:
-    for item in name_list:
-        f.write(item + "\n")
+def make_cartoon_text_file(args):
+    ### Making text files for all image names for human faces
+    root_dir = args.human_data_root_path
+    name_list = []
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            file_dir = os.path.join(root, file)
+            if file_dir.endswith(".jpg"):
+                # image = io.imread(file_dir)
+                # image = resize(image, (128,128))
+                # io.imsave("/Users/gerald/Desktop/GAN datasets/cartoonfaces/" + file ,image)
+                name_list.append(file)
+
+    with open("/content/drive/My Drive/CSC420Project/human.txt", 'w') as f:
+        for item in name_list:
+            f.write(item + "\n")
--- Scripts/train_test_split.py
+++ Scripts/train_test_split.py
@@ -1,32 +1,32 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from train import args
 
-### Splitting into train and test text files for human faces
+def split_human_data(args):
+    ### Splitting into train and test text files for human faces
 
-x = pd.read_csv("/content/drive/My Drive/CSC420Project/human.txt", sep=" ", header=None)[0].values.tolist()
-x = x[:32467] #shuffle data if you want randomeness 
-X_train, X_test= train_test_split(x, test_size=0.3, random_state=420)
+    x = pd.read_csv("/content/drive/My Drive/CSC420Project/human.txt", sep=" ", header=None)[0].values.tolist()
+    x = x[:32467] #shuffle data if you want randomeness 
+    X_train, X_test= train_test_split(x, test_size=0.3, random_state=420)
 
-with open(args.human_train_path, "w") as f:
-    for item in X_train:
-        f.write(item + "\n")
+    with open(args.human_train_path, "w") as f:
+        for item in X_train:
+            f.write(item + "\n")
 
-with open(args.human_test_path, "w") as f:
-    for item in X_test:
-        f.write(item + "\n")
+    with open(args.human_test_path, "w") as f:
+        for item in X_test:
+            f.write(item + "\n")
 
+def split_cartoon_data(args):
+    ### Splitting into train and test text files for cartoon faces
 
-### Splitting into train and test text files for cartoon faces
+    x = pd.read_csv("/content/drive/My Drive/CSC420Project/cartoon.txt", sep=" ", header=None)[0].values.tolist()
+    x = x[:100000] #shuffle data if you want randomeness 
+    X_train, X_test= train_test_split(x, test_size=0.3, random_state=420)
 
-x = pd.read_csv("/content/drive/My Drive/CSC420Project/cartoon.txt", sep=" ", header=None)[0].values.tolist()
-x = x[:100000] #shuffle data if you want randomeness 
-X_train, X_test= train_test_split(x, test_size=0.3, random_state=420)
+    with open(args.cartoon_train_path, "w") as f:
+        for item in X_train:
+            f.write(item + "\n")
 
-with open(args.cartoon_train_path, "w") as f:
-    for item in X_train:
-        f.write(item + "\n")
-
-with open(args.cartoon_test_path, "w") as f:
-    for item in X_test:
-        f.write(item + "\n")
+    with open(args.cartoon_test_path, "w") as f:
+        for item in X_test:
+            f.write(item + "\n")
