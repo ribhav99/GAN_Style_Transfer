a08e2b0b5fcaf5e82ef1df54687ebb2004c22863
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-06-22 22:35:33 +0530

    modified original

--- Scripts/Generator.py
+++ Scripts/Generator.py
@@ -5,24 +5,34 @@ import torch.nn as nn
 def init_downconv(input_channel, second_channel, num_layers):
     layers = []
     for i in range(3):
-        this_layer = [nn.Conv2d(input_channel, second_channel,3, padding =1), nn.BatchNorm2d(second_channel), nn.ReLU(), nn.MaxPool2d(2)]
+        this_layer = [nn.Conv2d(input_channel, second_channel, 3, padding=1), nn.BatchNorm2d(
+            second_channel), nn.ReLU(), nn.MaxPool2d(2)]
         layers += this_layer
         input_channel = second_channel
         second_channel = second_channel * 2
     return nn.Sequential(*layers)
 
-class Generator(nn.Module):
-    def __init__(self, args = None):
-        super(Generator, self).__init__()
-        self.downconv = init_downconv(3,16,3)
-        self.upconv1 = nn.Sequential(nn.Upsample(scale_factor = 2), nn.ConvTranspose2d(64,32,3,padding = 1),nn.BatchNorm2d(32), nn.ReLU())
-        self.upconv2 = nn.Sequential(nn.Upsample(scale_factor = 2), nn.ConvTranspose2d(32,16,3,padding = 1),nn.BatchNorm2d(16), nn.ReLU())
-        self.upconv3 = nn.Sequential(nn.Upsample(scale_factor = 2), nn.ConvTranspose2d(16,3,3,padding = 1), nn.Tanh())
 
-    def forward(self,x): 
+class Generator(nn.Module):
+    def __init__(self, args=None):
+        super(Generator, self).__init__()
+        self.downconv = init_downconv(3, 16, 3)
+        self.upconv1 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
+            64, 32, 3, padding=1), nn.BatchNorm2d(32), nn.ReLU())
+        self.upconv2 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
+            32, 16, 3, padding=1), nn.BatchNorm2d(16), nn.ReLU())
+        self.upconv3 = nn.Sequential(nn.Upsample(
+            scale_factor=2), nn.ConvTranspose2d(16, 3, 3, padding=1), nn.Tanh())
+
+    def forward(self, x):
         x = self.downconv(x)
         x = self.upconv1(x)
         x = self.upconv2(x)
         x = self.upconv3(x)
         return x
 
+
+y = torch.rand(2, 3, 128, 128)
+generator = Generator()
+y = generator(y)
+print(y.shape)
--- Scripts/GeneratorAndDiscriminator.py
+++ Scripts/GeneratorAndDiscriminator.py
@@ -1,3 +1,4 @@
+from attrdict import AttrDict
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,6 +14,7 @@ import numpy as np
 # Minimum image size is 64x64 otherwise max_pooling will crash
 # Will need to remove conv layers or remove their max_pooling
 
+
 class Discriminator(nn.Module):
 
     def __init__(self, args):
@@ -20,16 +22,21 @@ class Discriminator(nn.Module):
         self._linear_dim = None
 
         # only takes in RGB images
-        self.conv1 = nn.Sequential(nn.Conv2d(args.image_dimensions[2], args.features_d, args.kernel_size, stride=2, padding = 1),nn.BatchNorm2d(args.features_d) ,nn.ReLU(), nn.MaxPool2d(args.max_pool))
-        self.conv2 = nn.Sequential(nn.Conv2d(args.features_d, args.features_d * 2, args.kernel_size, stride=2, padding = 1),nn.BatchNorm2d(args.features_d * 2) ,nn.ReLU(), nn.MaxPool2d(args.max_pool))
-        self.conv3 = nn.Sequential(nn.Conv2d(args.features_d * 2, args.features_d * 4, args.kernel_size, stride=2, padding = 1),nn.BatchNorm2d(args.features_d * 4) ,nn.ReLU(), nn.MaxPool2d(args.max_pool))
-        
-        fake_data = torch.rand(args.image_dimensions).view(-1, args.image_dimensions[2], args.image_dimensions[0], args.image_dimensions[1])
+        self.conv1 = nn.Sequential(nn.Conv2d(args.image_dimensions[2], args.features_d, args.kernel_size, stride=2, padding=1), nn.BatchNorm2d(
+            args.features_d), nn.ReLU(), nn.MaxPool2d(args.max_pool))
+        self.conv2 = nn.Sequential(nn.Conv2d(args.features_d, args.features_d * 2, args.kernel_size, stride=2,
+                                             padding=1), nn.BatchNorm2d(args.features_d * 2), nn.ReLU(), nn.MaxPool2d(args.max_pool))
+        self.conv3 = nn.Sequential(nn.Conv2d(args.features_d * 2, args.features_d * 4, args.kernel_size,
+                                             stride=2, padding=1), nn.BatchNorm2d(args.features_d * 4), nn.ReLU(), nn.MaxPool2d(args.max_pool))
+
+        fake_data = torch.rand(args.image_dimensions).view(
+            -1, args.image_dimensions[2], args.image_dimensions[0], args.image_dimensions[1])
         self.conv(fake_data)
 
-        self.linear1 = nn.Sequential(nn.Linear(self._linear_dim, 150), nn.BatchNorm1d(150), nn.ReLU())
-        self.linear2 = nn.Sequential(nn.Linear(150,1), nn.Sigmoid())
-    
+        self.linear1 = nn.Sequential(
+            nn.Linear(self._linear_dim, 150), nn.BatchNorm1d(150), nn.ReLU())
+        self.linear2 = nn.Sequential(nn.Linear(150, 1), nn.Sigmoid())
+
     def conv(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
@@ -42,11 +49,13 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         x = self.conv(x)
+        # batch_size = x.shape[0]
+        # x = x.view(batch_size, -1)
+        # print(x.shape)
         x = x.view(-1, self._linear_dim)
         x = self.linear1(x)
         x = self.linear2(x)
         return x
-
 
 
 class Generator(nn.Module):
@@ -55,19 +64,25 @@ class Generator(nn.Module):
         super().__init__()
         self._linear_dim = None
 
-        self.conv1 = nn.Conv2d(args.cartoon_dimensions[2], args.features_g, args.kernel_size, padding =1), nn.BatchNorm2d(args.features_g), nn.ReLU(), nn.MaxPool2d(args.max_pool)
-        self.conv2 = nn.Conv2d(args.features_g, args.features_g * 2, args.kernel_size, padding =1), nn.BatchNorm2d(args.features_g * 2), nn.ReLU(), nn.MaxPool2d(args.max_pool)
-        self.conv3 = nn.Conv2d(args.features_g * 2, args.features_g * 4, args.kernel_size, padding =1), nn.BatchNorm2d(args.features_g * 4), nn.ReLU(), nn.MaxPool2d(args.max_pool)
+        self.conv1 = nn.Sequential(nn.Conv2d(args.cartoon_dimensions[2], args.features_g, args.kernel_size, padding=1), nn.BatchNorm2d(
+            args.features_g), nn.ReLU(), nn.MaxPool2d(args.max_pool))
+        self.conv2 = nn.Sequential(nn.Conv2d(args.features_g, args.features_g * 2, args.kernel_size,
+                                             padding=1), nn.BatchNorm2d(args.features_g * 2), nn.ReLU(), nn.MaxPool2d(args.max_pool))
+        self.conv3 = nn.Sequential(nn.Conv2d(args.features_g * 2, args.features_g * 4, args.kernel_size,
+                                             padding=1), nn.BatchNorm2d(args.features_g * 4), nn.ReLU(), nn.MaxPool2d(args.max_pool))
 
         # fake_data = torch.rand(args.cartoon_dimensions).view(-1, args.cartoon_dimensions[2], args.cartoon_dimensions[0], args.cartoon_dimensions[1])
         # self.down_conv(fake_data)
 
         # self.dense1 = nn.Linear(self._linear_dim, args.image_dimensions[0] * args.image_dimensions[1] * args.image_dimensions[2])
 
-        self.trans_conv1 = nn.Sequential(nn.Upsample(scale_factor = 2), nn.ConvTranspose2d(args.features_g * 4, args.features_g * 2, args.kernel_size, padding = 1), nn.BatchNorm2d(args.features * 2), nn.ReLU())
-        self.trans_conv2 = nn.Sequential(nn.Upsample(scale_factor = 2), nn.ConvTranspose2d(args.features_g * 2, args.features_g, args.kernel_size, padding = 1), nn.BatchNorm2d(args.features), nn.ReLU())
-        self.trans_conv3 = nn.Sequential(nn.Upsample(scale_factor = 2), nn.ConvTranspose2d(args.features_g, args.image_dimensions[2], args.kernel_size, padding = 1), nn.Tanh())
-    
+        self.trans_conv1 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
+            args.features_g * 4, args.features_g * 2, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g * 2), nn.ReLU())
+        self.trans_conv2 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
+            args.features_g * 2, args.features_g, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g), nn.ReLU())
+        self.trans_conv3 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
+            args.features_g, args.image_dimensions[2], args.kernel_size, padding=1), nn.Tanh())
+
     def down_conv(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
@@ -83,48 +98,52 @@ class Generator(nn.Module):
         x = self.trans_conv2(x)
         x = self.trans_conv3(x)
         return x
-    
+
     def forward(self, x):
         x = self.down_conv(x)
         x = self.up_conv(x)
         return x
 
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 # THESE VALUES FOR TESTING. REMOVE THEM
 
-from attrdict import AttrDict
 args = AttrDict()
 args_dict = {
-'learning_rate': 0.001,
-'batch_size' : 64,
-'image_dimensions' : (218, 178, 3), 
-'cartoon_dimensions' : (128, 128, 3),
-'max_pool' : (2, 2),
-'features_d' : 16,
-'features_g' : 16,
-'num_epochs' : 30,
-'kernel_size' : 3,
-'human_train_path' : "/content/GAN_Style_Transfer/data/human_train.txt",
-'human_test_path' : "/content/GAN_Style_Transfer/data/human_test.txt",
-'cartoon_train_path' : "/content/GAN_Style_Transfer/data/cartoon_train.txt",
-'cartoon_test_path' : "/content/GAN_Style_Transfer/data/cartoon_test.txt",
-'human_data_root_path' : "/content/humanfaces/",
-'cartoon_data_root_path' : "/content/cartoonfaces/",
-'save_path' : "/content/GAN_Style_Transfer/Models",
-'image_save_f' : 10, #i.e save an image every 10 epochs
-'use_wandb' : False
+    'learning_rate': 0.001,
+    'batch_size': 1,
+    'image_dimensions': (218, 178, 3),
+    'cartoon_dimensions': (128, 128, 3),
+    'max_pool': (2, 2),
+    'features_d': 16,
+    'features_g': 16,
+    'num_epochs': 30,
+    'kernel_size': 3,
+    'human_train_path': "/content/GAN_Style_Transfer/data/human_train.txt",
+    'human_test_path': "/content/GAN_Style_Transfer/data/human_test.txt",
+    'cartoon_train_path': "/content/GAN_Style_Transfer/data/cartoon_train.txt",
+    'cartoon_test_path': "/content/GAN_Style_Transfer/data/cartoon_test.txt",
+    'human_data_root_path': "/content/humanfaces/",
+    'cartoon_data_root_path': "/content/cartoonfaces/",
+    'save_path': "/content/GAN_Style_Transfer/Models",
+    'image_save_f': 10,  # i.e save an image every 10 epochs
+    'use_wandb': False
 }
 args.update(args_dict)
 
 
-x = torch.rand(args.image_dimensions).view(-1, 3, args.image_dimensions[0], args.image_dimensions[1])
+# x = torch.rand(args.image_dimensions).view(-1, 3,
+#                                            args.image_dimensions[0], args.image_dimensions[1])
+x = torch.rand(2, 3, 218, 178)
 
 disciminator = Discriminator(args)
 print(disciminator(x))
 
-y = torch.rand(args.cartoon_dimensions).view(-1, args.cartoon_dimensions[2], args.cartoon_dimensions[0], args.cartoon_dimensions[1])
+# y = torch.rand(args.cartoon_dimensions).view(-1,
+#                                              args.cartoon_dimensions[2], args.cartoon_dimensions[0], args.cartoon_dimensions[1])
+y = torch.rand(2, 3, 128, 128)
 generator = Generator(args)
-y = generator(y).view(-1, args.image_dimensions[0], args.image_dimensions[1], args.image_dimensions[2])
+y = generator(y)  # .view(-1, 2, args.image_dimensions[0],
+# args.image_dimensions[1], args.image_dimensions[2])
 print(y.shape)
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
