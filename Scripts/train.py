7c99262eff951d071944c4efec63c67129c49e98
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-06-23 17:56:58 +0530

    psh

--- Scripts/train.py
+++ Scripts/train.py
@@ -89,7 +89,7 @@ def train(args, device, wandb=None):
             del matrix_of_img
         total = batch_num + 1
         avg_loss_gen = loss_gen / total
-        avg_loss_discrim = loss_discrim*args.discrim_train_fgi / total
+        avg_loss_discrim = loss_discrim*args.discrim_train_f / total
         if args.use_wandb:
             wandb.log(
                 {"discriminator loss": avg_loss_discrim, 'epoch': epoch + 1})
