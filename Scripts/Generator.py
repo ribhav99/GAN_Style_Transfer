f3f3bf662477f824c3d030a43b01d46b3661fc96
Gerald.Shen <gerald.shen@mail.utoronto.ca>
2020-06-26 10:59:29 -0400

    generator is a big boy now

--- Scripts/Generator.py
+++ Scripts/Generator.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from init_helper import ResidualBlock, init_upconv, init_downconv
+from init_helper import ResidualBlock, init_upconv, init_downconv, downconv_block
 
 class Generator(nn.Module):
     def __init__(self, args=None):
@@ -10,11 +10,15 @@ class Generator(nn.Module):
         pool_type = 'max' if args is None else args.pool_type
         self.downconv = init_downconv(channel_list, act_fn, pool_type)
         self.resblock = ResidualBlock(64)
-        self.upconv = init_upconv(channel_list, act_fn, last_layer_act = 'tanh')
+        self.convmiddle = nn.Sequential(*downconv_block(64,128, act_fn, pool_type)) 
+        self.resblock1 = ResidualBlock(128)
+        self.upconv = init_upconv(channel_list + [128], act_fn, last_layer_act = 'tanh')
 
     def forward(self, x):
         x = self.downconv(x)
         x = self.resblock(x)
+        x = self.convmiddle(x)
+        x = self.resblock1(x)
         x = self.upconv(x)
         return x
 
