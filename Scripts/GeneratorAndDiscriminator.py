76b07657ff7f99fbb82706bbe96af7766a0f96f6
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-06-26 18:32:28 +0530

    more layers

--- Scripts/GeneratorAndDiscriminator.py
+++ Scripts/GeneratorAndDiscriminator.py
@@ -70,18 +70,32 @@ class Generator(nn.Module):
                                              padding=1), nn.BatchNorm2d(args.features_g * 2), args.activation(), args.pool(args.max_pool))
         self.conv3 = nn.Sequential(nn.Conv2d(args.features_g * 2, args.features_g * 4, args.kernel_size,
                                              padding=1), nn.BatchNorm2d(args.features_g * 4), args.activation(), args.pool(args.max_pool))
+        self.conv4 = nn.Sequential(nn.Conv2d(args.features_g * 4, args.features_g * 8, args.kernel_size,
+                                             padding=1), nn.BatchNorm2d(args.features_g * 8), args.activation(), args.pool(args.max_pool))
+        self.conv5 = nn.Sequential(nn.Conv2d(args.features_g * 8, args.features_g * 16, args.kernel_size,
+                                             padding=1), nn.BatchNorm2d(args.features_g * 16), args.activation(), args.pool(args.max_pool))
 
         self.trans_conv1 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
-            args.features_g * 4, args.features_g * 2, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g * 2), args.activation())
+            args.features_g * 16, args.features_g * 8, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g * 8), args.activation())
+
         self.trans_conv2 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
-            args.features_g * 2, args.features_g, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g), args.activation())
+            args.features_g * 8, args.features_g * 4, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g * 4), args.activation())
+
         self.trans_conv3 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
+            args.features_g * 4, args.features_g * 2, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g * 2), args.activation())
+
+        self.trans_conv4 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
+            args.features_g * 2, args.features_g, args.kernel_size, padding=1), nn.BatchNorm2d(args.features_g), args.activation())
+
+        self.trans_conv5 = nn.Sequential(nn.Upsample(scale_factor=2), nn.ConvTranspose2d(
             args.features_g, args.image_dimensions[2], args.kernel_size, padding=1), nn.Tanh())
 
     def down_conv(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
 
         if self._linear_dim is None:
             self._linear_dim = np.prod(x[0].shape)
@@ -92,6 +106,8 @@ class Generator(nn.Module):
         x = self.trans_conv1(x)
         x = self.trans_conv2(x)
         x = self.trans_conv3(x)
+        x = self.trans_conv4(x)
+        x = self.trans_conv5(x)
         return x
 
     def forward(self, x):
@@ -103,49 +119,46 @@ class Generator(nn.Module):
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 # THESE VALUES FOR TESTING. REMOVE THEM
 
-# args = AttrDict()
-# args_dict = {
-#     'dis_learning_rate': 0.001,
-#     'gen_learning_rate': 0.002,
-#     'image_dimensions': (128, 128, 1),
-#     'cartoon_dimensions': (128, 128, 1),
-#     'batch_size': 2,
-#     'max_pool': (2, 2),
-#     'features_d': 64,
-#     'features_g': 64,
-#     'num_epochs': 85,
-#     'kernel_size': 3,
-#     'human_train_path': "/content/GAN_Style_Transfer/data/human_train.txt",
-#     'human_test_path': "/content/GAN_Style_Transfer/data/human_test.txt",
-#     'cartoon_train_path': "/content/GAN_Style_Transfer/data/cartoon_train.txt",
-#     'cartoon_test_path': "/content/GAN_Style_Transfer/data/cartoon_test.txt",
-#     'human_data_root_path': "/content/humangray128/",
-#     'cartoon_data_root_path': "/content/cartoonfacesgray/",
-#     'save_path': "/content/GAN_Style_Transfer/Models",
-#     'image_save_f': 1,  # i.e save an image every 1 epochs
-#     'discrim_train_f': 3,
-#     'discrim_error_train': False,
-#     'pool': nn.AvgPool2d,
-#     'activation': nn.LeakyReLU,
-#     'use_wandb': True
-# }
-# args.update(args_dict)
+args = AttrDict()
+args_dict = {
+    'dis_learning_rate': 0.001,
+    'gen_learning_rate': 0.002,
+    'image_dimensions': (128, 128, 1),
+    'cartoon_dimensions': (128, 128, 1),
+    'batch_size': 2,
+    'max_pool': (2, 2),
+    'features_d': 64,
+    'features_g': 64,
+    'num_epochs': 85,
+    'kernel_size': 3,
+    'human_train_path': "/content/GAN_Style_Transfer/data/human_train.txt",
+    'human_test_path': "/content/GAN_Style_Transfer/data/human_test.txt",
+    'cartoon_train_path': "/content/GAN_Style_Transfer/data/cartoon_train.txt",
+    'cartoon_test_path': "/content/GAN_Style_Transfer/data/cartoon_test.txt",
+    'human_data_root_path': "/content/humangray128/",
+    'cartoon_data_root_path': "/content/cartoonfacesgray/",
+    'save_path': "/content/GAN_Style_Transfer/Models",
+    'image_save_f': 1,
+    'discrim_train_f': 3,
+    'discrim_error_train': False,
+    'pool': nn.AvgPool2d,
+    'activation': nn.LeakyReLU,
+    'use_wandb': True
+}
+args.update(args_dict)
 
 
-# x = torch.rand(args.batch_size,
-#                args.image_dimensions[2], args.image_dimensions[0], args.image_dimensions[1])
+x = torch.rand(args.batch_size,
+               args.image_dimensions[2], args.image_dimensions[0], args.image_dimensions[1])
 
-# disciminator = Discriminator(args)
-# x = disciminator(x)
-# print(x.shape)
-# # predictions = [1 if i > 0.5 else 0 for i in x]
-# # print(predictions)
-# # print(x)
+disciminator = Discriminator(args)
+x = disciminator(x)
+print(x.shape)
 
-# y = torch.rand(args.batch_size,
-#                args.cartoon_dimensions[2], args.cartoon_dimensions[0], args.cartoon_dimensions[1])
-# generator = Generator(args)
-# y = generator(y)  # .view(-1, 2, args.image_dimensions[0],
-# # args.image_dimensions[1], args.image_dimensions[2])
-# print(y.shape)
+
+y = torch.rand(args.batch_size,
+               args.cartoon_dimensions[2], args.cartoon_dimensions[0], args.cartoon_dimensions[1])
+generator = Generator(args)
+y = generator(y)
+print(y.shape)
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
