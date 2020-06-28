a1d46fc09f349ba819ae35d99af3f066006804fc
Gerald.Shen <gerald.shen@mail.utoronto.ca>
2020-06-28 11:30:23 -0400

    indents

--- poggers/init_helper.py
+++ poggers/init_helper.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 
 class SINNode(nn.Module):
-    def __init__(self): super(SINNode, self).__init__()
+    def __init__(self): 
+        super(SINNode, self).__init__()
 
     def forward(self,x):
         return torch.sin(x)
