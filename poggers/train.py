b4b7c34d482df867afbd8e38a71967edb0aee5ad
Gerald.Shen <gerald.shen@mail.utoronto.ca>
2020-06-27 22:36:12 -0400

    made train more efficient and used 2 optimizers instead

--- poggers/train.py
+++ poggers/train.py
@@ -1,4 +1,5 @@
 import torch
+import itertools
 import torch.nn as nn
 import torch.optim as optim
 from Generator import Generator
@@ -15,25 +16,34 @@ def train(args, wandb = None):
     d_y = Discriminator(args).to(device)
     g_x_y = Generator(args).to(device) #cartoon -> human faces
     g_y_x = Generator(args).to(device) #human faces -> cartoon
-    optimiser_d_x = optim.Adam(d_x.parameters(), lr=args.dis_learning_rate)
-    optimiser_d_y = optim.Adam(d_y.parameters(), lr=args.dis_learning_rate)
-    optimiser_g_x_y = optim.Adam(g_x_y.parameters(), lr=args.gen_learning_rate)
-    optimiser_g_y_x = optim.Adam(g_y_x.parameters(), lr=args.gen_learning_rate)
+    optimiser_d = optim.Adam(itertools.chain(d_x.parameters(), d_y.parameters()),lr=args.dis_learning_rate)
+    optimiser_g = optim.Adam(itertools.chain(g_x_y.parameters(), g_y_x.parameters()),lr=args.gen_learning_rate)
     if args.use_wandb:
         wandb.watch(d_x, log='all')
         wandb.watch(d_y, log='all')
         wandb.watch(g_x_y, log = 'all')
         wandb.watch(g_y_x, log = 'all')
+    def dis_loss_backward(net, real, fake):
+        loss_real = ((net(real) -1)**2).mean() 
+        loss_fake = (net(fake)**2).mean()
+        total_loss = (loss_real + loss_fake) *0.5
+        total_loss.backward()
+        return total_loss
     def cycle_loss(real, reconstructed):
         loss = (torch.abs(real - reconstructed)).mean()
         return args.lambda_cycle * loss 
+    def set_grad(nets, req_grad):
+        for net in nets:
+            for param in net.parameters():
+                param.requires_grad = requires_grad
     d_x.train()
     d_y.train()
     g_x_y.train()
     g_y_x.train()
     print("Start Training....")
     for epoch in trange(args.num_epochs):
-        total_d_loss = 0.0
+        total_d_x_loss = 0.0
+        total_d_y_loss = 0.0
         total_g_x_y_loss = 0.0
         total_g_y_x_loss = 0.0
         total_data = 0
@@ -42,28 +52,24 @@ def train(args, wandb = None):
             total_data += x.shape[0]
             fake_x = g_y_x(y)
             fake_y = g_x_y(x)
-            optimiser_d_x.zero_grad()
-            optimiser_d_y.zero_grad()
-            d_loss_real = ((d_x(x) - 1) **2).mean() + ((d_y(y) - 1)**2).mean()
-            d_loss_fake = (d_y(fake_y.detach())**2).mean() + (d_x(fake_x.detach())**2).mean()
-            total_d = (d_loss_real + d_loss_fake) * 0.5
-            total_d.backward()
-            optimiser_d_x.step()
-            optimiser_d_y.step()
-            optimiser_g_x_y.zero_grad()
-            optimiser_g_y_x.zero_grad()
-# loss_g_y_x = ((d_x(fake_x) - 1)**2).mean() + cycle_loss(y, g_x_y(fake_x))
-# loss_g_x_y = ((d_y(fake_y) - 1)**2).mean() + cycle_loss(x, g_y_x(fake_y))
-            loss_g_y_x = ((d_x(fake_x) - 1)**2).mean() + cycle_loss(y, g_x_y(fake_x).detach())
-            loss_g_x_y = ((d_y(fake_y) - 1)**2).mean() + cycle_loss(x, g_y_x(fake_y).detach())
+            set_grad([d_x,d_y], True)
+            optimiser_d.zero_grad()
+            d_x_loss = dis_loss_backward(d_x, x, fake_x.detach()).item()
+            d_y_loss = dis_loss_backward(d_y, y, fake_y.detach()).item()
+            optimiser_d.step()
+            set_grad([d_x,d_y], False) #don't need grad on the weights of the discriminators here
+            optimiser_g.zero_grad()
+            loss_g_y_x = ((d_x(fake_x) - 1)**2).mean() + cycle_loss(y, g_x_y(fake_x))
+            loss_g_x_y = ((d_y(fake_y) - 1)**2).mean() + cycle_loss(x, g_y_x(fake_y))
             loss_g_y_x.backward()
             loss_g_x_y.backward()
-            optimiser_g_x_y.step()
-            optimiser_g_y_x.step()
-            total_d_loss += (total_d.item() * 2)
+            optimiser_g.step()
+            total_d_x_loss += (d_x_loss * 2)
+            total_d_y_loss += (d_y_loss * 2)
             total_g_x_y_loss += loss_g_x_y.item()
             total_g_y_x_loss += loss_g_y_x.item()
-        avg_d_loss = total_d_loss / total_data
+        avg_d_x_loss = total_d_x_loss / total_data
+        avg_d_y_loss = total_d_y_loss / total_data
         avg_g_x_y_loss = total_g_x_y_loss / total_data
         avg_g_y_x_loss = total_g_y_x_loss / total_data
         if args.use_wandb:
@@ -71,10 +77,12 @@ def train(args, wandb = None):
             name = "examples " + "epoch " + str(epoch + 1)
             wandb.log({name : [wandb.Image(matrix_of_img[i]) for i in range(10)]})
             del matrix_of_img
-            wandb.log({"Avg Discriminator loss": avg_d_loss, 'epoch': epoch + 1})
+            wandb.log({"Avg Discriminator for Cartoon loss": avg_d_x_loss, 'epoch': epoch + 1})
+            wandb.log({"Avg Discriminator for Human Faces loss": avg_d_y_loss, 'epoch': epoch + 1})
             wandb.log({"Avg Cartoon to Human loss": avg_g_x_y_loss, 'epoch': epoch + 1})
             wandb.log({"Avg Human to Cartoon loss": avg_g_y_x_loss, 'epoch': epoch + 1})
-        print("Avg Discriminator Loss: {}".format(avg_d_loss))
+        print("Avg Discriminator for Cartoon Loss: {}".format(avg_d_x_loss))
+        print("Avg Discriminator for Human Loss: {}".format(avg_d_y_loss))
         print("Avg Cartoon to Human Loss: {}".format(avg_g_x_y_loss))
         print("Avg Human to Cartoon Loss: {}".format(avg_g_y_x_loss))
     if args.use_wandb:
